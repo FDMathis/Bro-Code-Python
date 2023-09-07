31deced7468d8df6a8ad49bc8ce6445a2b0e142c
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-07 17:22:29 -0600

    Python-setup

--- Variables/index.py
+++ Variables/index.py
@@ -1,2 +1,3 @@
 print("Hello World!")
 print("Why am I getting errors?")
+print("Nevermind.  I fixed it under code runner settings, under Extension settings>Executor Map>Python> and I added a 3 after the second python word on the code before the -u")
