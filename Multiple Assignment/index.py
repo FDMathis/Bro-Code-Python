13b1b98ab4562d3684d391ff2fdf3ca82d0a00ed
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-08 16:39:58 -0600

    Bro - Multiple Assign

---
+++ Multiple Assignment/index.py
@@ -0,0 +1,26 @@
+# multiple assignment = allows us to assign variables at the same 
+#                       time in one line of code
+
+# Single Assignment
+name = "Spopngebob"
+age = 21
+attractive = False
+
+print(name)
+print(age)
+print(attractive)
+
+#Multiple Assignment
+name, age, attractive = "Patrick", 25, True
+
+print(name)
+print(age)
+print(attractive)
+
+#Another example with multiple people
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
