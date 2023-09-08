aa07b585c0af6031c6657415a41f4e4fc0badcae
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-07 18:34:33 -0600

    Bro - Variables

--- Variables/index.py
+++ Variables/index.py
@@ -1,3 +1,32 @@
-print("Hello World!")
-print("Why am I getting errors?")
-print("Nevermind.  I fixed it under code runner settings, under Extension settings>Executor Map>Python> and I added a 3 after the second python word on the code before the -u")
+# variable = a container for a value. Behaves as the value it contains.
+
+#Strings
+first_name = "Kyzer"
+last_name = "Sose"
+full_name = first_name + " " + last_name
+print("Hello " + full_name)
+
+#How to assess what class of data an item is
+print(type(full_name))
+
+#Intergers
+age = 21
+age += 1
+print(age)
+print(type(age))
+
+# How to concatenate strings and intergers
+print("You are " + str(age) + " years old!")
+
+#Floats
+height = 250.5
+
+print("Your height is " + str(height) + " !")
+print(type(height))
+
+
+#Booleans
+human = True
+print(human)
+print(type(human))
+print("Are you a human: " + str(human))
