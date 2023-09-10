82b4ed1d8fb2a75ae194572aa4ea194bd04953b7
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-09 18:11:30 -0600

    BRO -TYPECASTING

---
+++ Typecast/index.py
@@ -0,0 +1,13 @@
+# typecasting = convert the data type of a value into another data type
+
+x = 1       # interger
+y = 2.0     #float
+z = "3"     # string
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x)
+print(y)
+print(z*3)
