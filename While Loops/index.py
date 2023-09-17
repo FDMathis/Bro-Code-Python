0fd9f54eef1c536350f979053a16c09e4cded444
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-17 12:04:50 -0600

    bro for loops

--- While Loops/index.py
+++ While Loops/index.py
@@ -1,9 +1,10 @@
 # while loop =  a statement that will execute it's block of code,
 #               as long as it's condition remains true
 
-while 1 == 1:
-    print("Help! I'm stuck in a loop!")
+#while 1 == 1:
+    #print("Help! I'm stuck in a loop!")
+
 
 while name len(name) == 0:
-    name = input("Enter your name.")
+    name = (input("Enter your name."))
 print("Hello, " +name)
