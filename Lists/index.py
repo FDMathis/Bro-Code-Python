3c0db0d4bfd94591414bc794f0e08875e00b9596
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-23 18:12:49 -0600

    Bro lists

---
+++ Lists/index.py
@@ -0,0 +1,26 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza", "cheeseburger", "hotdog", "spaghetti", "pudding"]
+print(food)
+
+food[0] = "sushi"
+print(food)
+
+food.append("ice cream")
+print(food)
+
+food.remove("hotdog")
+print(food)
+
+food.pop()
+print(food)
+
+food.insert(0, "cake")
+print(food)
+
+food.sort()
+print(food)
+
+food.clear()
+print(food)
+
