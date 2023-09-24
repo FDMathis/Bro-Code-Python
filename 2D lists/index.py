72b99f12e9d71c8f634d47d830be192a28b977f1
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-23 18:19:40 -0600

    Bro 2D lists

---
+++ 2D lists/index.py
@@ -0,0 +1,9 @@
+# 2D list = a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "cheeseburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
