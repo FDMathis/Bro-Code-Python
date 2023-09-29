88c9947d1580e6ac2f1afa0ba2d2eaf06945dcd0
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-28 21:16:16 -0600

    Bro Return stats

---
+++ Return Statements/index.py
@@ -0,0 +1,10 @@
+# return statement = Functions that send values/objects back to the caller.
+#                    These values/objects are know as the function's "return value"
+
+def multiply(number1,number2):
+    result = number1 * number2
+    return result
+
+x = multiply(6,8)
+
+print(x)
