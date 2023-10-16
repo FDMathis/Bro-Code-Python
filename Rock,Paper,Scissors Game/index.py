73530ad197e25ad19102e88f6e2f4a9023dd2e48
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-10-16 11:25:49 -0600

    Bro RPS game complete

--- Rock,Paper,Scissors Game/index.py
+++ Rock,Paper,Scissors Game/index.py
@@ -16,4 +16,41 @@ while True:
         print("Tie!")
 
     elif player == "rock":
-        
+        if computer == "paper":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You lose")
+        if computer == "scissors":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You win!")
+
+    elif player == "scissors":
+        if computer == "rock":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You lose")
+        if computer == "paper":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You win!")
+
+    elif player == "paper":
+        if computer == "scissors":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You lose")
+        if computer == "rock":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You win!")
+
+     play_again = input("Play again (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+print("Bye!") 
+
+
+
+    
